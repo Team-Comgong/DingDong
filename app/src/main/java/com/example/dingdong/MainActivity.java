4bc88a5fba70e359b35d8aed3a5ea029548e1da5
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-06-11 19:25:45 +0900

    FCM 토큰 생성 및 DB 처리

--- app/src/main/java/com/example/dingdong/MainActivity.java
+++ app/src/main/java/com/example/dingdong/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.dingdong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("getInstanceId failed", task.getException());
+                            return;
+                        }
 
-        myRef.setValue("Hello, World!");
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        Log.d("My token is : ", token);
+
+                        DatabaseReference mDatabase;// ...
+                        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                        mDatabase.child("Device_ID").push().setValue(token);
+                    }
+                });
     }
 }
